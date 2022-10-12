56b4c1e69d0d7618848ab0108a180dcf5406ec95
Carmyo <carmelodicristina@gmail.com>
2022-10-12 18:25:40 +0200

    done

--- esercitazione.php
+++ esercitazione.php
@@ -1,7 +1,22 @@
 <?php
 $text = "Striking from a fortress hidden among the billion stars of the galaxy, rebel spaceships have won their first victory in a battle with the powerful Imperial Starfleet. The EMPIRE fears that another defeat could bring a thousand more solar systems into the rebellion, and Imperial control over the galaxy would be lost forever.";
-$censored = str_replace("rebel", "classified", $text);
+$toHide = $_GET["censura"];
+
+
 ?>
+
+
+<?php
+$censored = str_replace($toHide, "classified", $text);
+?>
+
+
+
+
+
+
 <p><?= $text?></p>
 <p><?=strlen($text);?></p>
-<p><?=str_replace("rebel", "classified", $text)?>;</p>
+<p>
+    <?= $censored ?>
+</p>
