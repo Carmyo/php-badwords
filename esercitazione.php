d3f966f8c1ac96b9137395f2ce6ca4455987f089
Carmyo <carmelodicristina@gmail.com>
2022-10-13 15:58:19 +0200

    create array

--- esercitazione.php
+++ esercitazione.php
@@ -1,12 +1,6 @@
 <?php
 $text = "Striking from a fortress hidden among the billion stars of the galaxy, rebel spaceships have won their first victory in a battle with the powerful Imperial Starfleet. The EMPIRE fears that another defeat could bring a thousand more solar systems into the rebellion, and Imperial control over the galaxy would be lost forever.";
-$toHide = $_GET["censura"];
-
-
-?>
-
-
-<?php
+$toHide = $_GET['censura'];
 $censored = str_replace($toHide, "classified", $text);
 ?>
 
@@ -15,8 +9,18 @@ $censored = str_replace($toHide, "classified", $text);
 
 
 
-<p><?= $text?></p>
-<p><?=strlen($text);?></p>
+
+
+
+<p>
+    <?= $text?>
+</p>
+<p>
+    <?=strlen($text);?>
+</p>
 <p>
     <?= $censored ?>
+</p>
+<p>
+    <?=strlen($censored);?>
 </p>
